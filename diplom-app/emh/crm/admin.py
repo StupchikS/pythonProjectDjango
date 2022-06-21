61d52a451f20dd38a77424840341efffd4534910
StupchikS <mser25@mail.ru>
2022-06-21 11:38:36 +0800

    добавил поля в модель crm формы и админка

--- diplom-app/emh/crm/admin.py
+++ diplom-app/emh/crm/admin.py
@@ -10,12 +10,12 @@ class Comment(admin.StackedInline):
 
 
 class OrderAdm(admin.ModelAdmin):
-    list_display = ('id', 'order_status', 'order_name', 'order_phone', 'order_dt')
-    list_display_links = ('id', 'order_name')
-    search_fields = ('id', 'order_name', 'order_phone', 'order_dt')
+    list_display = ('id', 'order_status', 'order_name', 'order_phone', 'order_email', 'order_context', 'order_dt')
+    list_display_links = ('id', 'order_name', 'order_context')
+    search_fields = ('id', 'order_name', 'order_phone', 'order_email', 'order_dt')
     list_filter = ('order_status',)
     list_editable = ('order_status', 'order_phone')
-    fields = ('id', 'order_status', 'order_name', 'order_phone', 'order_dt')
+    fields = ('id', 'order_status', 'order_name', 'order_phone', 'order_email', 'order_context', 'order_dt')
     readonly_fields = ('id', 'order_dt')
     list_per_page = 10
     list_max_show_all = 50
--- diplom-app/emh/crm/models.py
+++ diplom-app/emh/crm/models.py
@@ -2,28 +2,30 @@ from django.db import models
 
 
 class StatusCrm(models.Model):  # статус заявки
-    status_name = models.CharField(max_length=200, verbose_name='Название статуса')
+    status_name = models.CharField(max_length=200, verbose_name='Название статуса обращения')
 
     def __str__(self):
         return self.status_name
 
     class Meta:
-        verbose_name = 'Статус'
-        verbose_name_plural = 'Статусы'
+        verbose_name = 'Статус обращения'
+        verbose_name_plural = 'Статусы обращений'
 
 
 class Order(models.Model):  # Заказы
     order_dt = models.DateTimeField(auto_now=True, verbose_name='Дата создания')
     order_name = models.CharField(max_length=200, verbose_name='Имя')
-    order_phone = models.CharField(max_length=200, verbose_name='Телефон')
+    order_phone = models.IntegerField(max_length=11, verbose_name='Телефон')
+    order_email = models.EmailField(null=True, blank=True, verbose_name='Электронная почта')
+    order_context = models.CharField(max_length=500, null=True, blank=True, verbose_name='Сообщение')
     order_status = models.ForeignKey(StatusCrm, on_delete=models.PROTECT, null=True, blank=True, verbose_name='Статус')
 
     def __str__(self):
         return self.order_name
 
     class Meta:
-        verbose_name = 'Заказ'
-        verbose_name_plural = 'Заказы'
+        verbose_name = 'Обращение'
+        verbose_name_plural = 'Обращения'
 
 
 class CommentsCrm(models.Model):
