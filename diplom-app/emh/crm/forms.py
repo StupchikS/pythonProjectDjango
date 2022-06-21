3951126a53a1af11327b5996da1c55f0b41bed23
StupchikS <mser25@mail.ru>
2022-06-21 12:13:46 +0800

    выправил шаблон осталось сделать телеграм настройки

--- diplom-app/emh/crm/forms.py
+++ diplom-app/emh/crm/forms.py
@@ -3,6 +3,6 @@ from django import forms
 
 class OrderForm(forms.Form):
     name = forms.CharField(max_length=200, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    phone = forms.IntegerField(max_length=11, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    phone = forms.IntegerField(widget=forms.TextInput(attrs={'class': 'form-control'}))
     email = forms.EmailField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
     context = forms.CharField(max_length=500, widget=forms.Textarea(attrs={'class': 'form-control'}))
